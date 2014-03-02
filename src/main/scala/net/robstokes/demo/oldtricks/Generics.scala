073d4ed6b64e8d2f8584356367367de9ec88c0e0
Rob Stokes <rob.lee.stokes@gmail.com>
2014-03-02 14:51:32 -0600

    Saving work.

--- src/main/scala/net/robstokes/demo/oldtricks/Generics.scala
+++ src/main/scala/net/robstokes/demo/oldtricks/Generics.scala
@@ -1,13 +1,16 @@
 package net.robstokes.demo.oldtricks
 
 class Generics {
-
-  class Stack[T] {
+  class SuperStack[T] {
     var elems: List[T] = Nil
     def push(x: T) { elems = x :: elems }
     def top: T = elems.head
     def pop() { elems = elems.tail }
   }
+
+
+
+
 
 
 
